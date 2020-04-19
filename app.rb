bbdc0533e37539a67d80065194029f616413eb97
Gor <7495500@gmail.com>
2020-04-19 17:32:03 +0300

    Set data results as hash

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-# require 'sqlite3'
+require 'sqlite3'
 
-# configure do
-# 	db = get_db
-# 	db.execute CREATE TABLE IF NOT EXISTS "Users" (
-# 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-# 	 	"username" TEXT,
-# 	 	"phone" TEXT,
-# 	 	"datestamp" TEXT,
-# 	 	"barber" TEXT,
-# 	 	"color" TEXT
-# 	);
-# end
+configure do
+	db = get_db
+	db.execute CREATE TABLE IF NOT EXISTS "Users" (
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 	"username" TEXT,
+	 	"phone" TEXT,
+	 	"datestamp" TEXT,
+	 	"barber" TEXT,
+	 	"color" TEXT
+	);
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -57,13 +57,19 @@ post '/visit' do
 
 	end
 
-	# db = get_db
-	# db.execute 'INSERT INTO Users (username, phone, datestamp, barber, color)
-	# VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+	db = get_db
+	db.execute 'INSERT INTO Users (username, phone, datestamp, barber, color)
+	VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
 	erb :visit
 end
 
-# def get_db
-# 	return SQLite3::Database.new 'sinatra_barber_shop.sqlite'
-# end
+get '/showusers' do
+
+end
+
+def get_db
+	db = SQLite3::Database.new 'sinatra_barber_shop.sqlite'
+	db.results_as_hash = true
+	return db
+end
